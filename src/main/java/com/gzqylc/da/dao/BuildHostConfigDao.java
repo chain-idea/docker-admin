04adf4f1c418d61f1c4098b0aa85af4b7d4a0802
jiangtao <hustme@126.com>
2021-02-20 16:54:17 +0800

    主机排序

---
+++ src/main/java/com/gzqylc/da/dao/BuildHostConfigDao.java
@@ -0,0 +1,9 @@
+package com.gzqylc.da.dao;
+
+import com.gzqylc.da.entity.BuildHostConfig;
+import com.gzqylc.lang.web.base.BaseDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BuildHostConfigDao extends BaseDao<BuildHostConfig> {
+}
---
+++ src/main/java/com/gzqylc/da/entity/BuildHostConfig.java
@@ -0,0 +1,29 @@
+package com.gzqylc.da.entity;
+
+import com.gzqylc.lang.web.base.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+/**
+ * 指定构建主机
+ */
+@Getter
+@Setter
+@Entity
+public class BuildHostConfig extends BaseEntity {
+
+    @NotNull
+    @ManyToOne
+    Host host;
+
+    int seq;
+
+    // 局域网的git地址，替换公网地址，加快速度
+    String lanGitHost;
+
+
+}
