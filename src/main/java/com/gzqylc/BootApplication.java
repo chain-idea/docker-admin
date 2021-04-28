ef70619d3134a886e69b6e5d0280fef26ecb307d
jiangtao <hustme@126.com>
2021-04-28 18:40:25 +0800

    测试docker file 2

--- src/main/java/com/gzqylc/BootApplication.java
+++ src/main/java/com/gzqylc/BootApplication.java
@@ -1,11 +1,11 @@
 package com.gzqylc;
 
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@Slf4j
+
 @SpringBootApplication
 @EnableAsync
 public class BootApplication {
